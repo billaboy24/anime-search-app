c3508276229958ac9059f5a9ac9ec2df00308422
kathirvan <kathirvandev@gmail.com>
2025-05-06 21:08:11 +0800

    chore: create custom hook to retrieve initial data

--- src/hooks/useInitialAnime.js
+++ src/hooks/useInitialAnime.js
@@ -2,16 +2,17 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-const fetchTopAnime = async () => {
-  const res = await axios.get(
-    "https://api.jikan.moe/v4/top/anime?filter=airing"
-  );
+const fetchTopAnime = async ({ queryKey }) => {
+  const [, page] = queryKey;
+  const res = await axios.get(`https://api.jikan.moe/v4/top/anime`, {
+    params: { filter: "airing", page },
+  });
   return res.data;
 };
 
-export const useInitialAnime = () => {
+export const useInitialAnime = (page) => {
   return useQuery({
-    queryKey: ["initialAnime"],
+    queryKey: ["initialAnime", page],
     queryFn: fetchTopAnime,
     staleTime: 1000 * 60 * 5,
   });
