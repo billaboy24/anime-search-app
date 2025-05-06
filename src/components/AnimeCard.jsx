a990c3556f4929b9874e6f7b88e3254c5ff5b55d
kathirvan <kathirvandev@gmail.com>
2025-05-06 20:42:52 +0800

    feat: anime card

---
+++ src/components/AnimeCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, Typography, CardActionArea } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const AnimeCard = ({ anime }) => {
+  const navigate = useNavigate();
+
+  return (
+    <Card onClick={() => navigate(`/anime/${anime.mal_id}`)} sx={{ borderRadius: 3, height: '100%' }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="300"
+          image={anime.images?.jpg?.image_url}
+          alt={anime.title}
+        />
+        <CardContent>
+          <Typography variant="h6" noWrap>{anime.title}</Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+export default AnimeCard;
